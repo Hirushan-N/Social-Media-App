b90b6d57edecc75a49b3817545d58cbf39944efe
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-24 14:21:33 +0530

    created user model

---
+++ API/src/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: { type: String, required : true },
+    dob: { type: String, required : true },
+    gender: { type: String, required : true },
+    profileImage: { type: String, required : false },
+    email : { type: String, required : true, unique:true , match: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/},
+    password : { type: String, required : true }
+});
+
+module.exports = mongoose.model('User', userSchema);
