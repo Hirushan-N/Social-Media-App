5aae79ae10e2d411ae584fe29fa7410dfe140b26
Hirushan-N <nadeeshhirushan@gmail.com>
2023-01-27 14:10:58 +0530

    Update postService.js

--- client/src/services/postService.js
+++ client/src/services/postService.js
@@ -5,6 +5,10 @@ class postService{
     getAllPosts(){
         return axios.get('http://localhost:5000/posts/');
     }
+
+    createPost(post){
+        return axios.post('http://localhost:5000/posts/',post);
+    }
 }
 
 export default new postService();
